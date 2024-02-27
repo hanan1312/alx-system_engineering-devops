c2257f93555bdea45f94abd60eaf377b15143e8b
hanan1312 <nana.elmogey@gmail.com>
2024-02-28 01:00:49 +0200

    bash

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,22 +1,24 @@
 #!/usr/bin/env ruby
+input=$(echo $1 | sed 's/\[from:\(.*\)\] \[to:\(.*\)\] \[flags:\(.*\)\]/echo \1,\2,\3/e')
+echo $input
 
-if ARGV.empty?
-    puts "Please provide an argument."
-    exit
-  end
+# if ARGV.empty?
+#     puts "Please provide an argument."
+#     exit
+#   end
   
-  input = ARGV[0]
+#   input = ARGV[0]
   
-  pattern = /\[from:(?<from>.*?)\] \[to:(?<to>.*?)\] \[flags:(?<flags>.*?)\]/
+#   pattern = /\[from:(?<from>.*?)\] \[to:(?<to>.*?)\] \[flags:(?<flags>.*?)\]/
   
-  match_data = input.match(pattern)
+#   match_data = input.match(pattern)
   
-  if match_data
-    from = match_data[:from]
-    to = match_data[:to]
-    flags = match_data[:flags]
-    puts "From: #{from}, To: #{to}, Flags: #{flags}"
-  else
-    puts "No match found."
-  end
+#   if match_data
+#     from = match_data[:from]
+#     to = match_data[:to]
+#     flags = match_data[:flags]
+#     puts "From: #{from}, To: #{to}, Flags: #{flags}"
+#   else
+#     puts "No match found."
+#   end
   
