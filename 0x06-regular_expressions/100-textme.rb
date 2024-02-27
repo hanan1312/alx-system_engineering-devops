232b10011002c1765f731b2253358a6d5935c95e
hanan1312 <nana.elmogey@gmail.com>
2024-02-28 01:02:03 +0200

    bash

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,24 +1,6 @@
 #!/usr/bin/env ruby
-input=$(echo $1 | sed 's/\[from:\(.*\)\] \[to:\(.*\)\] \[flags:\(.*\)\]/echo \1,\2,\3/e')
-echo $input
-
-# if ARGV.empty?
-#     puts "Please provide an argument."
-#     exit
-#   end
-  
-#   input = ARGV[0]
-  
-#   pattern = /\[from:(?<from>.*?)\] \[to:(?<to>.*?)\] \[flags:(?<flags>.*?)\]/
-  
-#   match_data = input.match(pattern)
-  
-#   if match_data
-#     from = match_data[:from]
-#     to = match_data[:to]
-#     flags = match_data[:flags]
-#     puts "From: #{from}, To: #{to}, Flags: #{flags}"
-#   else
-#     puts "No match found."
-#   end
-  
+# puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+input = ARGV[0]
+matches = input.scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
+output = matches.map { |match| match.join(',') }.join(',')
+puts output
