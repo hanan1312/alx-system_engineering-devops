6710068a8c787dd96dc831367de4dfe3fe91b0e2
hanan1312 <nana.elmogey@gmail.com>
2024-02-29 00:23:29 +0200

    only answers

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-# puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
 input = ARGV[0]
 matches = input.scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
 output = matches.map { |match| match.join(',') }.join(',')
