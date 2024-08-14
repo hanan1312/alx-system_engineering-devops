058545897ab52824e045705aa99ec92bd6de1e45
hanan1312 <nana.elmogey@gmail.com>
2024-08-15 00:47:45 +0300

    update

--- 0x16-api_advanced/tests/0-main.py
+++ 0x16-api_advanced/tests/0-main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-0-main
-"""
 import sys
+
 
 if __name__ == '__main__':
     number_of_subscribers = __import__('0-subs').number_of_subscribers
@@ -10,3 +8,4 @@ if __name__ == '__main__':
         print("Please pass an argument for the subreddit to search.")
     else:
         print("{:d}".format(number_of_subscribers(sys.argv[1])))
+
