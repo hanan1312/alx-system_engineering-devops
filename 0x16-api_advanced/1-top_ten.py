f64d0e387fd0085fedd23445c7414f7831692ed5
hanan1312 <nana.elmogey@gmail.com>
2024-08-15 00:50:14 +0300

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""queries the Reddit API and prints the titles of the first 10 hot"""
-
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """gets the titles of the 10 hottest posts on a given subreddit."""
+    """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
@@ -20,5 +19,4 @@ def top_ten(subreddit):
         return
     results = response.json().get("data")
     [print(c.get("data").get("title")) for c in results.get("children")]
-
 
